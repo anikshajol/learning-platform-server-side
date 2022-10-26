e8e0e3b04ec3ed5402243e90fdda52693807922d
anikshajol <anikshajol@gmail.com>
2022-10-27 00:00:00 +0600

    dynamic id created

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+
+const products = require("./Data/product.json");
+
+app.use(cors());
+
+const Port = process.env.Port || 5000;
+
+app.listen(Port, () => {
+  console.log("object", Port);
+});
+
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
+
+app.get("/course", (req, res) => {
+  res.send(products);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = products.find((product) => product.id == id);
+  res.send(selectedCourse);
+});
